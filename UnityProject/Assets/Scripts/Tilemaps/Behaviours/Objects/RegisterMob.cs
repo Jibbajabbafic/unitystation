8616ba5ef16ce96eea697e26ac2ea0b4b4410123
Jibby <Jibbajabbafic@users.noreply.github.com>
2020-01-31 10:23:30 +0000

    WIP RegisterMob

---
+++ UnityProject/Assets/Scripts/Tilemaps/Behaviours/Objects/RegisterMob.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+using Mirror;
+
+/// <summary>
+/// <see cref="RegisterTile"/> for an object, adds additional logic to
+/// make object passable / impassable.
+/// </summary>
+[RequireComponent(typeof(Directional))]
+[RequireComponent(typeof(SpriteMatrixRotation))]
+[ExecuteInEditMode]
+public class RegisterMob : RegisterTile
+{
+	public bool AtmosPassable = true;
+	[SyncVar]
+	public bool Passable = true;
+
+	public override bool IsPassable(Vector3Int from, bool isServer)
+	{
+		return Passable || (isServer ? LocalPositionServer == TransformState.HiddenPos : LocalPositionClient == TransformState.HiddenPos );
+	}
+
+	public override bool IsPassable(bool isServer)
+	{
+		return Passable || (isServer ? LocalPositionServer == TransformState.HiddenPos : LocalPositionClient == TransformState.HiddenPos );
+	}
+
+	public override bool IsAtmosPassable(Vector3Int from, bool isServer)
+	{
+		return AtmosPassable || (isServer ? LocalPositionServer == TransformState.HiddenPos : LocalPositionClient == TransformState.HiddenPos );
+	}
+
+	#region UI Mouse Actions
+
+	public void OnHoverStart()
+	{
+		//thanks stack overflow!
+		Regex r = new Regex(@"
+				(?<=[A-Z])(?=[A-Z][a-z]) |
+				 (?<=[^A-Z])(?=[A-Z]) |
+				 (?<=[A-Za-z])(?=[^A-Za-z])", RegexOptions.IgnorePatternWhitespace);
+
+		string tmp = r.Replace(name, " ");
+		UIManager.SetToolTip = tmp;
+	}
+
+	public void OnHoverEnd()
+	{
+		UIManager.SetToolTip = "";
+	}
+
+	#endregion
+}
